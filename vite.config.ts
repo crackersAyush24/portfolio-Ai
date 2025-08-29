7bcb7e57cff75c79ba53fb9de4cc696490323eb9
Ayush Vinodkumar Chaubey <chaubeyayush04@gmail.com>
2025-08-29 21:15:31 +0530

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-Ai/', // 👈 This must match your repo name
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/portfolio-Ai/', // 👈 this should match your repo name
+})
