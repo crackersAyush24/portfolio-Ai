319c81798d1ba4af9d40665083f8b0f714690e41
Ayush Vinodkumar Chaubey <chaubeyayush04@gmail.com>
2025-08-30 13:16:48 +0530

    Fix routing and base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio-ai/", // <<-- Add this
   plugins: [react()],
+  base: "/portfolio-Ai/"  // <-- this is important
 });
