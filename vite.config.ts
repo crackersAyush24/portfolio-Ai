dd23caa20df522103bfa1b92afa606500ec80eb6
crackersAyush24 <chaubeyayush04@gmail.com>
2025-08-30 12:53:34 +0530

    Update GH Pages workflow"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio-ai/',  // <-- important for GH Pages
   plugins: [react()],
-  base: '/portfolio-Ai/', // 👈 your repo name here
 });
