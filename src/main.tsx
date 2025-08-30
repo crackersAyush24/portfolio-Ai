65f3c0250948b5e60944fbb048b0fe6b7606be02
Ayush Vinodkumar Chaubey <chaubeyayush04@gmail.com>
2025-08-30 13:09:54 +0530

    Setup auto-deploy workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-// main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
